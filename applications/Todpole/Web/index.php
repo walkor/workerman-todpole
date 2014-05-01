7907701cda809d2cb84c731779c2de85726634b4
walkor <worker-man@qq.com>
2014-05-01 22:31:51 +0800

    样式调整

--- applications/Todpole/Web/index.php
+++ applications/Todpole/Web/index.php
@@ -34,11 +34,11 @@
 				       <a rel="external" href="http://github.com/walkor/workerman-todpole" title="workerman-todpole at GitHub"><img src="css/images/github.png" alt="fork on github"></a>
 			</section>
 			
-				<section id="wtf">
-				   <h2>后端使用&nbsp;<a rel="external" href="http://workerman.net/workerman-todpole" target="_blank">workerman</a></h2>
-				</section>
 				<section id="concept">
 					<h2>前端界面来自&nbsp;<a href="http://rumpetroll.com/" target="_blank">rumpetroll.com</a></h2>
+				</section>
+				<section id="wtf">
+				   <h2>后端使用&nbsp;<a rel="external" href="http://workerman.net/workerman-todpole" target="_blank">workerman</a></h2>
 				</section>
 			</aside>
             <aside id="frogMode">
