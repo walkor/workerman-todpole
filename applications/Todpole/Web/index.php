d722865ccd39c52b18bd0cf6249609c3c6cef237
walkor <worker-man@qq.com>
2014-05-02 23:25:23 +0800

    解决首页函数重定义错误

--- applications/Todpole/Web/index.php
+++ applications/Todpole/Web/index.php
@@ -98,10 +98,13 @@
 </html>
 
 <?php 
-function is_mobile()
+if(!function_exists('is_mobile'))
 {
-    //php判断客户端是否为手机
-    $agent = $_SERVER['HTTP_USER_AGENT'];
-    return (strpos($agent,"NetFront") || strpos($agent,"iPhone") || strpos($agent,"MIDP-2.0") || strpos($agent,"Opera Mini") || strpos($agent,"UCWEB") || strpos($agent,"Android") || strpos($agent,"Windows CE") || strpos($agent,"SymbianOS"));
+    function is_mobile()
+    {
+        //php判断客户端是否为手机
+        $agent = $_SERVER['HTTP_USER_AGENT'];
+        return (strpos($agent,"NetFront") || strpos($agent,"iPhone") || strpos($agent,"MIDP-2.0") || strpos($agent,"Opera Mini") || strpos($agent,"UCWEB") || strpos($agent,"Android") || strpos($agent,"Windows CE") || strpos($agent,"SymbianOS"));
+    }
 }
 ?>
