e55dfd8a23ed11a5714100a251e6156df49587b6
walkor <worker-man@qq.com>
2014-05-09 01:10:16 +0800

    前端配置说明

--- applications/Todpole/Web/js/Settings.js
+++ applications/Todpole/Web/js/Settings.js
@@ -1,9 +1,10 @@
+// 此文件下载者不用更改，兼容其他域名使用
 var Settings = function() {
 	// 如果是workerman.net phpgame.cn域名 则采用多个接入端随机负载均衡
 	var domain_arr = ['workerman.net', 'phpgame.cn','www.workerman.net', 'www.phpgame.cn'];
 	if(0 >= $.inArray(document.domain, domain_arr))
 	{
-		this.socketServer = domain_arr[Math.floor(Math.random() * domain_arr.length + 1)-1];
+		this.socketServer = 'ws://'+domain_arr[Math.floor(Math.random() * domain_arr.length + 1)-1]+':8280';
 	}
 	else
 	{
