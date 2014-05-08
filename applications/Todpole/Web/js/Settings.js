455abfbe27155ad16c2c5336618b86bbeb36f56a
walkor <worker-man@qq.com>
2014-05-09 00:46:58 +0800

    前端支持多个服务端接入

--- applications/Todpole/Web/js/Settings.js
+++ applications/Todpole/Web/js/Settings.js
@@ -1,3 +1,13 @@
 var Settings = function() {
-  this.socketServer = 'ws://'+document.domain+':8280';
+	// 如果是workerman.net phpgame.cn域名 则采用多个接入端随机负载均衡
+	var domain_arr = ['workerman.net', 'phpgame.cn','www.workerman.net', 'www.phpgame.cn'];
+	if($.inArray(document.domain, domain_arr))
+	{
+		this.socketServer = domin_arr[Math.floor(Math.random() * Arr.length + 1)-1];
+	}
+	else
+	{
+		// 运行在其它域名上
+		this.socketServer = 'ws://'+document.domain+':8280';
+	}
 }
