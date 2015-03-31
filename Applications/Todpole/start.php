a822b30e190514342089e4b0662165662993cefa
walkor <walkor@users.noreply.github.com>
2015-03-31 12:58:07 +0800

    Update start.php

--- Applications/Todpole/start.php
+++ Applications/Todpole/start.php
@@ -3,6 +3,7 @@ use \Workerman\Worker;
 use \Workerman\WebServer;
 use \GatewayWorker\Gateway;
 use \GatewayWorker\BusinessWorker;
+use \Workerman\Autoloader;
 
 // 自动加载类
 require_once __DIR__ . '/../../Workerman/Autoloader.php';
