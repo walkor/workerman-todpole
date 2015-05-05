64edf0023fe56dff60492a62f19086f371a3cc3f
walkor <worker-man@qq.com>
2015-05-06 00:08:33 +0800

    mit

--- Applications/Todpole/Event.php
+++ Applications/Todpole/Event.php
@@ -1,10 +1,20 @@
 <?php
 /**
- * 
+ * This file is part of workerman.
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the MIT-LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author walkor<walkor@workerman.net>
+ * @copyright walkor<walkor@workerman.net>
+ * @link http://www.workerman.net/
+ * @license http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+
+/**
  * 主逻辑
  * 主要是处理 onMessage onClose 三个方法
- * @author walkor <walkor@workerman.net>
- * 
  */
 
 use \GatewayWorker\Lib\Gateway;
