b6bd5673a71408838cf14d0df486207377c5f777
walkor <walkor@users.noreply.github.com>
2015-05-21 14:11:21 +0800

    Update start_businessworker.php

--- Applications/Todpole/start_businessworker.php
+++ Applications/Todpole/start_businessworker.php
@@ -18,7 +18,7 @@ use \GatewayWorker\BusinessWorker;
 use \Workerman\Autoloader;
 
 // 自动加载类
-require_once __DIR__ . '/../../workerman-for-win/Autoloader.php';
+require_once __DIR__ . '/../../Workerman/Autoloader.php';
 Autoloader::setRootPath(__DIR__);
 
 
