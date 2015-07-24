d7eaffa2875f80c10aa28dbf526f3d6d7a131520
walkor <walkor@users.noreply.github.com>
2015-07-24 14:12:12 +0800

    Update Settings.js

--- Applications/Todpole/Web/js/Settings.js
+++ Applications/Todpole/Web/js/Settings.js
@@ -1,7 +1,7 @@
 // 此文件下载者不用更改，兼容其他域名使用
 var Settings = function() {
 	// 如果是workerman.net phpgame.cn域名 则采用多个接入端随机负载均衡
-	var domain_arr = ['workerman.net', 'phpgame.cn','www.workerman.net', 'www.phpgame.cn'];
+	var domain_arr = ['workerman.net', 'www.workerman.net'];
 	if(0 <= $.inArray(document.domain, domain_arr))
 	{
 		this.socketServer = 'ws://'+domain_arr[Math.floor(Math.random() * domain_arr.length + 1)-1]+':8282';
