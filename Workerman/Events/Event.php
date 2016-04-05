d8c231824cca37acec155f7b6624935d1defb2c8
walkor <worker-man@qq.com>
2016-04-05 21:42:24 +0800

    update workerman core

--- Workerman/Events/Event.php
+++ Workerman/Events/Event.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 /**
  * This file is part of workerman.
  *
@@ -6,135 +6,152 @@
  * For full copyright and license information, please see the MIT-LICENSE.txt
  * Redistributions of files must retain the above copyright notice.
  *
- * @author  有个鬼<42765633@qq.com>
- * @link    http://www.workerman.net/
- * @license http://www.opensource.org/licenses/mit-license.php MIT License
+ * @author    有个鬼<42765633@qq.com>
+ * @copyright 有个鬼<42765633@qq.com>
+ * @link      http://www.workerman.net/
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT License
  */
 namespace Workerman\Events;
 
 /**
- * ev eventloop
+ * libevent eventloop
  */
-class Ev implements EventInterface
+class Event implements EventInterface
 {
     /**
+     * Event base.
+     * @var object
+     */
+    protected $_eventBase = null;
+    
+    /**
      * All listeners for read/write event.
-     *
      * @var array
      */
     protected $_allEvents = array();
-
+    
     /**
      * Event listeners of signal.
-     *
      * @var array
      */
     protected $_eventSignal = array();
-
+    
     /**
      * All timer event listeners.
      * [func, args, event, flag, time_interval]
-     *
      * @var array
      */
     protected $_eventTimer = array();
 
     /**
      * Timer id.
-     *
      * @var int
      */
     protected static $_timerId = 1;
-
+    
     /**
-     * Add a timer.
-     * {@inheritdoc}
+     * construct
+     * @return void
      */
-    public function add($fd, $flag, $func, $args = null)
+    public function __construct()
     {
-        $callback = function ($event, $socket) use ($fd, $func) {
-            try {
-                call_user_func($func, $fd);
-            } catch (\Exception $e) {
-                echo $e;
-                exit(250);
-            } catch (\Error $e) {
-                echo $e;
-                exit(250);
-            }
-        };
-
+        $this->_eventBase = new \EventBase();
+    }
+   
+    /**
+     * @see EventInterface::add()
+     */
+    public function add($fd, $flag, $func, $args=array())
+    {
         switch ($flag) {
             case self::EV_SIGNAL:
-                $event                   = new \EvSignal($fd, $callback);
-                $this->_eventSignal[$fd] = $event;
+
+                $fd_key = (int)$fd;
+                $event = \Event::signal($this->_eventBase, $fd, $func);
+                if (!$event||!$event->add()) {
+                    return false;
+                }
+                $this->_eventSignal[$fd_key] = $event;
                 return true;
+
             case self::EV_TIMER:
             case self::EV_TIMER_ONCE:
-                $repeat                             = $flag == self::EV_TIMER_ONCE ? 0 : $fd;
-                $param                              = array($func, (array)$args, $flag, $fd, self::$_timerId);
-                $event                              = new \EvTimer($fd, $repeat, array($this, 'timerCallback'), $param);
+
+                $param = array($func, (array)$args, $flag, $fd, self::$_timerId);
+                $event = new \Event($this->_eventBase, -1, \Event::TIMEOUT|\Event::PERSIST, array($this, "timerCallback"), $param);
+                if (!$event||!$event->addTimer($fd)) {
+                    return false;
+                }
                 $this->_eventTimer[self::$_timerId] = $event;
                 return self::$_timerId++;
+                
             default :
-                $fd_key                           = (int)$fd;
-                $real_flag                        = $flag === self::EV_READ ? \Ev::READ : \Ev::WRITE;
-                $event                            = new \EvIo($fd, $real_flag, $callback);
+                $fd_key = (int)$fd;
+                $real_flag = $flag === self::EV_READ ? \Event::READ | \Event::PERSIST : \Event::WRITE | \Event::PERSIST;
+                $event = new \Event($this->_eventBase, $fd, $real_flag, $func, $fd);
+                if (!$event||!$event->add()) {
+                    return false;
+                }
                 $this->_allEvents[$fd_key][$flag] = $event;
                 return true;
         }
-
     }
-
+    
     /**
-     * Remove a timer.
-     * {@inheritdoc}
+     * @see Events\EventInterface::del()
      */
     public function del($fd, $flag)
     {
         switch ($flag) {
+
             case self::EV_READ:
             case self::EV_WRITE:
+
                 $fd_key = (int)$fd;
                 if (isset($this->_allEvents[$fd_key][$flag])) {
-                    $this->_allEvents[$fd_key][$flag]->stop();
+                    $this->_allEvents[$fd_key][$flag]->del();
                     unset($this->_allEvents[$fd_key][$flag]);
                 }
                 if (empty($this->_allEvents[$fd_key])) {
                     unset($this->_allEvents[$fd_key]);
                 }
                 break;
+
             case  self::EV_SIGNAL:
+
                 $fd_key = (int)$fd;
                 if (isset($this->_eventSignal[$fd_key])) {
-                    $this->_allEvents[$fd_key][$flag]->stop();
+                    $this->_allEvents[$fd_key][$flag]->del();
                     unset($this->_eventSignal[$fd_key]);
                 }
                 break;
+
             case self::EV_TIMER:
             case self::EV_TIMER_ONCE:
                 if (isset($this->_eventTimer[$fd])) {
-                    $this->_eventTimer[$fd]->stop();
+                    $this->_eventTimer[$fd]->del();
                     unset($this->_eventTimer[$fd]);
                 }
                 break;
         }
         return true;
     }
-
+    
     /**
      * Timer callback.
-     *
-     * @param \EvWatcher $event
+     * @param null $fd
+     * @param int $what
+     * @param int $timer_id
      */
-    public function timerCallback($event)
+    public function timerCallback($fd, $what, $param)
     {
-        $param    = $event->data;
         $timer_id = $param[4];
+        
         if ($param[2] === self::EV_TIMER_ONCE) {
-            $this->_eventTimer[$timer_id]->stop();
+            $this->_eventTimer[$timer_id]->del();
             unset($this->_eventTimer[$timer_id]);
         }
+
         try {
             call_user_func_array($param[0], $param[1]);
         } catch (\Exception $e) {
@@ -145,27 +162,25 @@ class Ev implements EventInterface
             exit(250);
         }
     }
-
+    
     /**
-     * Remove all timers.
-     *
+     * @see Events\EventInterface::clearAllTimer() 
      * @return void
      */
     public function clearAllTimer()
     {
         foreach ($this->_eventTimer as $event) {
-            $event->stop();
+            $event->del();
         }
         $this->_eventTimer = array();
     }
+     
 
     /**
-     * Main loop.
-     *
      * @see EventInterface::loop()
      */
     public function loop()
     {
-        \Ev::run();
+        $this->_eventBase->loop();
     }
 }
