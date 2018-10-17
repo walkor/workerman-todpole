73ece19ec852dc7604ce84e587c1265decb914d5
walkor <walkor@users.noreply.github.com>
2018-10-17 18:45:11 +0800

    Update start_for_win.bat

--- start_for_win.bat
+++ start_for_win.bat
@@ -1,2 +1,3 @@
+CHCP 65001
 php Applications\Todpole\start_register.php Applications\Todpole\start_web.php Applications\Todpole\start_gateway.php Applications\Todpole\start_businessworker.php
 pause
