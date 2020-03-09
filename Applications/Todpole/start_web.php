68036ba263c7b5c594ffed01cbbcb267c8321530
walkor <walkor@users.noreply.github.com>
2020-03-09 20:34:29 +0800

    Update start_web.php

--- Applications/Todpole/start_web.php
+++ Applications/Todpole/start_web.php
@@ -12,19 +12,64 @@
  * @license http://www.opensource.org/licenses/mit-license.php MIT License
  */
 use \Workerman\Worker;
-use \Workerman\WebServer;
-use \GatewayWorker\Gateway;
-use \GatewayWorker\BusinessWorker;
-use \Workerman\Autoloader;
+use Workerman\Protocols\Http\Request;
+use Workerman\Protocols\Http\Response;
+use Workerman\Connection\TcpConnection;
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 
 // WebServer
-$web = new WebServer("http://0.0.0.0:8383");
+$web = new Worker("http://0.0.0.0:8383");
 // WebServer数量
 $web->count = 2;
-// 设置站点根目录
-$web->addRoot('www.your_domain.com', __DIR__.'/Web');
+
+$web->name = 'web';
+
+define('WEBROOT', __DIR__ . '/Web');
+
+$web->onMessage = function (TcpConnection $connection, Request $request) {
+    $path = $request->path();
+    if ($path === '/') {
+        $connection->send(exec_php_file(WEBROOT.'/index.php', $request));
+        return;
+    }
+    $file = realpath(WEBROOT. $path);
+    if (false === $file) {
+        $connection->send(new Response(404, array(), '<h3>404 Not Found</h3>'));
+        return;
+    }
+    // Security check! Very important!!!
+    if (strpos($file, WEBROOT) !== 0) {
+        $connection->send(new Response(400));
+        return;
+    }
+    if (\pathinfo($file, PATHINFO_EXTENSION) === 'php') {
+        $connection->send(exec_php_file($file), $request);
+        return;
+    }
+
+    if (!empty($if_modified_since = $request->header('if-modified-since'))) {
+        // Check 304.
+        $info = \stat($file);
+        $modified_time = $info ? \date('D, d M Y H:i:s', $info['mtime']) . ' ' . \date_default_timezone_get() : '';
+        if ($modified_time === $if_modified_since) {
+            $connection->send(new Response(304));
+            return;
+        }
+    }
+    $connection->send((new Response())->withFile($file));
+};
+
+function exec_php_file($file, $request) {
+    \ob_start();
+    // Try to include php file.
+    try {
+        include $file;
+    } catch (\Exception $e) {
+        echo $e;
+    }
+    return \ob_get_clean();
+}
 
 
 // 如果不是在根目录启动，则运行runAll方法
